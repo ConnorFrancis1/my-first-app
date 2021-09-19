9f3c4bc03da9289b8338ea4316df3c49e8fa8b1d
ConnorFrancis1 <connorfrancis1999@icloud.com>
2021-09-18 19:06:37 -0500

    updated

---
+++ src/app/server/server.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  // selector: '[app-server]',
+  selector: 'app-servers',
+  templateUrl: './server.component.html'
+})
+
+export class ServerComponent {
+
+}
